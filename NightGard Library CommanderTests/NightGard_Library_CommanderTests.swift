5c1ec546cf39d1ad75bec22f8caa82b3ab829c42
Michael Fluharty <michael@fluharty.com>
2026-04-18 09:56:50 -0500

    Initial commit: NightGard Library Commander scaffold

---
+++ NightGard Library CommanderTests/NightGard_Library_CommanderTests.swift
@@ -0,0 +1,18 @@
+//
+//  NightGard_Library_CommanderTests.swift
+//  NightGard Library CommanderTests
+//
+//  Created by Michael Fluharty on 4/18/26.
+//
+
+import Testing
+
+struct NightGard_Library_CommanderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
